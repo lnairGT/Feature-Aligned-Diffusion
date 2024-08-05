183950b55e798da30d9b90797c33588f7dd5e42c
Lakshmi Nair <luckynitt@gmail.com>
2024-08-05 10:53:29 -0400

    Cleanup

--- gen_utils.py
+++ gen_utils.py
@@ -46,7 +46,6 @@ def generate_img(
 
             ni = ni.clip(0, 1)
             mean = torch.mean(ni)
-            print(mean)
 
         images.append(ni)
 
--- train-sd.sh
+++ train-sd.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python train_sd.py \
+python train_sd.py \
     --config config.yml \
     --num-img-gens 5 \
     --do-train \
